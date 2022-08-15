81afaf77a9842dd3ee1c078460af2f12b999f8a6
nsbendre25 <u154020@s099-n008.devcloud-edge>
2022-08-14 18:31:36 -0700

    Added Arguments for Job Submissions

--- cloud_training/ov-classify.py
+++ cloud_training/ov-classify.py
@@ -1,41 +1,63 @@
+import argparse
+import sys
 from openvino.runtime import Core
 import cv2
 import numpy as np
 import time
 
-input_mean=0
-input_std=255
 
-ie = Core()
-classification_model_xml = "./models/resnet_50/FP32/saved_model.xml"
 
-model = ie.read_model(model=classification_model_xml)
-compiled_model = ie.compile_model(model=model, device_name="CPU")
+def build_argparser():
+    """ Build argument parser.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model', required=True, type=str,
+                        help="Required. Path to an .xml file with a trained model")
+    parser.add_argument('-i', '--input', type=str,
+                        help='Required. Path to the dataset file in .pickle format.')
+    parser.add_argument("-o", "--output", type = str, required=True,
+                        help = "Path to output directory")
+    parser.add_argument("-d", "--device", type=str,
+        help="specify a device to infer on (the list of available devices is shown below). Default is CPU")
+  
+    return parser
 
-input_layer = compiled_model.input(0)
-output_layer = compiled_model.output(0)
+def main(args):
+    input_mean=0
+    input_std=255
+    ie = Core()
+    model = ie.read_model(args.model)
+    compiled_model = ie.compile_model(model=model, device_name=args.device)
 
-image_filename = "./inference_images/401.jpg"
-image = cv2.imread(image_filename)
-N, H, W, C = input_layer.shape
-resized_image = cv2.resize(src=image, dsize=(W, H))
-resized_image = np.asarray(resized_image, np.float32)
-normalized_image = (resized_image - input_mean) / input_std
-input_data = np.expand_dims(resized_image, 0)
+    input_layer = compiled_model.input(0)
+    output_layer = compiled_model.output(0)
 
-# warmup iterations
-for _ in range(5):
+    # image_filename = "./inference_images/401.jpg"
+    image = cv2.imread(args.input)
+    N, H, W, C = input_layer.shape
+    resized_image = cv2.resize(src=image, dsize=(W, H))
+    resized_image = np.asarray(resized_image, np.float32)
+    normalized_image = (resized_image - input_mean) / input_std
+    input_data = np.expand_dims(resized_image, 0)
+
+    # warmup iterations
+    for _ in range(5):
+        results = compiled_model([input_data])[output_layer]
+
+    start = time.time()
     results = compiled_model([input_data])[output_layer]
+    elapsed = time.time() - start
+    fps = 1 / elapsed
+    print(f'FPS: %.2f ' % fps)
+    print(f'Inference time: %.2f ms' % (elapsed * 1000))
 
-start = time.time()
-results = compiled_model([input_data])[output_layer]
-elapsed = time.time() - start
-fps = 1 / elapsed
-print(f'FPS: %.2f ' % fps)
-print(f'Inference time: %.2f ms' % (elapsed * 1000))
-
-index = np.argmax(results)
-if index == 0:
-    print("Found Cat")
-else:
-    print("Found Dog")
+    index = np.argmax(results)
+    if index == 0:
+        print("Found Cat")
+    else:
+        print("Found Dog")
+        
+if __name__ == "__main__":
+    args = build_argparser().parse_args()
+    sys.exit(main(args) or 0)
+    main(args)
--- cloud_training/tf-classify.py
+++ cloud_training/tf-classify.py
@@ -3,6 +3,19 @@ import time
 import numpy as np
 import tensorflow as tf
 from PIL import Image
+import argparse
+import sys
+
+def build_argparser():
+    """ Build argument parser.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str,
+                        help='Required. Path to the dataset file in .pickle format.')
+    parser.add_argument("-o", "--output", type = str, required=True,
+                        help = "Path to output directory")
+  
+    return parser
 
 def preprocess_image(frame,
                      input_height=299,
@@ -17,19 +30,26 @@ def preprocess_image(frame,
     result = np.expand_dims(normalized_image, 0)
     return result
 
-model = tf.saved_model.load("000000001")
-input_img_path = "./inference_images/401.jpg"
-frame = cv2.imread(input_img_path)
+def main(args):
 
-t = tf.convert_to_tensor(preprocess_image(frame, input_height=180, input_width=180))
+    model = tf.saved_model.load("000000001")
+    # input_img_path = "./inference_images/401.jpg"
+    frame = cv2.imread(args.input)
 
-# warmup iterations
-for _ in range(5):
+    t = tf.convert_to_tensor(preprocess_image(frame, input_height=180, input_width=180))
+
+    # warmup iterations
+    for _ in range(5):
+        results = model(t)
+
+    start = time.time()
     results = model(t)
-
-start = time.time()
-results = model(t)
-elapsed = time.time() - start
-fps = 1 / elapsed
-print(f'FPS: %.2f fps' % fps)
-print(f'Inference time: %.2f ms' % (elapsed * 1000))
+    elapsed = time.time() - start
+    fps = 1 / elapsed
+    print(f'FPS: %.2f fps' % fps)
+    print(f'Inference time: %.2f ms' % (elapsed * 1000))
+    
+if __name__ == "__main__":
+    args = build_argparser().parse_args()
+    sys.exit(main(args) or 0)
+    main(args)
