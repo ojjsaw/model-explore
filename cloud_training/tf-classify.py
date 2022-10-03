0a5a61ce5201e3ee90874f735762a31a5acf386d
Nihar Bendre <nihar.bendre@intel.com>
2022-10-03 11:33:50 -0700

    Final Updates Done

--- cloud_training/tf-classify.py
+++ cloud_training/tf-classify.py
@@ -9,6 +9,7 @@ import os
 import json
 import glob
 
+
 def build_argparser():
     """ Build argument parser.
     """
@@ -34,21 +35,13 @@ def append_value(dict_obj, key, value):
         # As key is not in dict,
         # so, add key-value pair
         dict_obj[key] = value
-        
 
-def preprocess_image(image,
-                     input_height=299,
-                     input_width=299,
-                     input_mean=0,
-                     input_std=255):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = Image.fromarray(image)
-    resized_image = image.resize((input_height, input_width))
-    resized_image = np.asarray(resized_image, np.float32)
-    normalized_image = (resized_image - input_mean) / input_std
-    result = np.expand_dims(normalized_image, 0)
-    return result
+def preprocess_image(frame, input_height, input_width, input_mean=0, input_std=255):
+    image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    resized_image = np.asarray(image.resize((input_height, input_width)), np.float32)
+    return np.expand_dims(resized_image, 0)
 
+    
 def main(args):
 
     model = tf.saved_model.load("000000001")
@@ -70,85 +63,48 @@ def main(args):
     for x in all_classes_folders:
         for file in glob.glob(f'{x}*.jpg'):
             images_path.append(file)
-    # print(images_path)
-    
-    
-    # image = cv2.imread(args.input)
-    
-    # prepare input images
+            
     ts = []
     for n in images_path:
         np_img = preprocess_image(cv2.imread(n), input_height=180, input_width=180)
         ts.append(tf.convert_to_tensor(np_img))
 
-    # t = tf.convert_to_tensor(preprocess_image(image, input_height=180, input_width=180))
-    
-    label_data = {}
+    # load labels
     with open('000000001/labels.txt') as f:
-        lines = f.read().splitlines()
-    for i in range(0, len(lines)):
-        append_value(label_data, 'labels', lines[i])
+        label_data = [line.rstrip() for line in f]
 
+   
     # warmup iterations
     for _ in range(5):
         results = model(ts[0])
 
-    # measure inference
+# measure inference
     total_elapsed = 0
-    for i in range(len(images_path)):
+    for i in range(len(ts)):
         start = time.time()
         results = model(ts[i])
         elapsed = time.time() - start
         total_elapsed += elapsed
-    
-    
-    # start = time.time()
-    # results = model(t)
-    # print('results: {}'.format(results))
-    # scores, class_ids = tf.math.top_k(results, k=len(label_data['labels']), sorted=True)
-    # elapsed = time.time() - start
-    # fps = 1 / elapsed
-    # print(f'FPS: %.2f fps' % fps)
-    # print(f'Inference time: %.2f ms' % (elapsed * 1000))
-    # for score, class_id in zip(scores[0], class_ids[0]):
-    #     score = score.numpy()
-        # print('Prediction: {}, {}'.format(label_data['labels'][class_id], score))
-        print ('Result: {}'.format(results[0]))
-        res = list(results[0])
-        print('res :{}'.format(res))
         index = np.argmax(results)
-        print('ArgMAX: {}'.format(np.argmax(results[0][0])))
-        # max_value = max(results[0])
-        # index = results[0].index(max_value)
-        print ('index: {}'.format(index))
-        # print ('Results for {}: {}'.format(images_path[i],results))
-        # prediction = lines[index]
-        print('Prediction for {}: {}'.format(images_path[i],lines[index]))
+        print('Prediction for {}: {}'.format(images_path[i],label_data[index]))
         print('Score for {}: {:.3g}'.format(images_path[i],max(results[0])))
-
+        
         image = cv2.imread(images_path[i])
-        # print(image)
         x,y,w,h = 0,0,image.shape[0], int(image.shape[0]/8)
         cv2.rectangle(image, (x, x), (x + w, y + h), (0,0,0), -1)
-        cv2.putText(image, "Prediction: {}, {:.3g}".format(lines[index], max(results[0])), (x + int(w/20),y + int(h/2)), cv2.FONT_HERSHEY_SIMPLEX, (image.shape[0]/500), (255, 255, 255), 1)
+        cv2.putText(image, "Prediction: {}, {:.3g}".format(label_data[index], max(results[0])), (x + int(w/20),y + int(h/2)), cv2.FONT_HERSHEY_SIMPLEX, (image.shape[0]/500), (255, 255, 255), 1)
         cv2.imwrite(os.path.join(output_path, f'prediction_{i}.jpg'), image)
-        # print ('Writing Image')
     
     avg_elapsed = total_elapsed/len(ts)
     fps = 1 / avg_elapsed
-    print(f'FPS: %.2f ' % fps)
-    print(f'Inference time: %.2f ms' % (avg_elapsed * 1000))    
+    print(f'Average FPS: %.2f ' % fps)
+    print(f'Average Inference time: %.2f ms' % (avg_elapsed * 1000))    
     job_id = str(os.environ['PBS_JOBID']).split('.')[0]
-
-
     with open(os.path.join(output_path, f'stats_{job_id}.txt'), 'w') as f:
         f.write('{:.3g} \n'.format(fps))
         f.write('{:.3g} \n'.format(elapsed * 1000))
-        # f.write('{} \n'.format(lines[index]))
         
 
-
-    
 if __name__ == "__main__":
     args = build_argparser().parse_args()
     sys.exit(main(args) or 0)
